550371c25d1701ace727663543a434b35ec389fe
Kimberly Chuc <kimmchuc@gmail.com>
2021-04-20 14:45:03 -1000

    set up pages and dynamic end points, as well as adding the dummy data

---
+++ pages/events/[...slug].js
@@ -0,0 +1,9 @@
+function FilteredEventsPage() {
+    return(
+        <div>
+            <h1>page that shows events based on filter</h1>
+        </div>
+    );
+}
+
+export default FilteredEventsPage;
---
+++ pages/events/index.js
@@ -0,0 +1,9 @@
+function AllEvents() {
+    return(
+        <div>
+            <h1>all events page</h1>
+        </div>
+    );
+}
+
+export default AllEvents;
