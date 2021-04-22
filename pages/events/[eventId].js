160f7ef9eddafd606c43787a9387bc76e27a0dc9
Kimberly Chuc <kimmchuc@gmail.com>
2021-04-21 15:42:14 -1000

    added event-detail folder

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,4 +1,16 @@
-function EventDetailPage(){
+import { useRouter } from 'next/router';
+import { getEventById } from '../../dummy-data';
+
+function EventDetailPage(props){
+    const router = useRouter();
+
+    const eventId = router.query.eventId;
+    const event = getEventById(eventId);
+
+    if (!event) {
+        return <p>no event found</p>
+    }
+
     return(
         <div>
             <h1>event detail page</h1>
